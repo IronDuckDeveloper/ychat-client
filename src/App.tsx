ec00a9f2aa0e39b877240b4d8bed2c62e25a935a
Egor Lachimov <lachimov@lifehack.studio>
2026-09-10 10:21:58 +0700

    Deploy to Git

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <NetworkOverlay />
         <Routes>
           <Route path="/" element={<Auth />} />
